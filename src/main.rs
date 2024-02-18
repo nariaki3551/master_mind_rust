191b5d2bcc253fe95187969a4bf9475a4d2df8dd
nariaki3551 <nariaki3551@gmail.com>
2024-02-18 18:24:01 +0900

    introduce clap argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,71 @@
+use clap::Parser;
+use itertools::Itertools;
 use std::io::Write;
 
 type Code = Vec<usize>;
 type CodeSet = Vec<Code>;
 type Hint = (usize, usize);
 
+#[derive(Debug)]
+struct Context {
+    color_num: usize,
+    pin_num: usize,
+    duplicate: bool,
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(help = "number of colors")]
+    color_num: usize,
+    #[arg(help = "number of pins")]
+    pin_num: usize,
+    #[arg(short, long, help = "codes do not have duplicate colors")]
+    non_duplicate: bool,
+}
+
 fn main() {
-    // temporary setting
-    let pin_num = 2; // number of pins
-    let color_num = 3; // number of colors
-    let duplicate = false; // code dose not have same colors
-    println!(
-        "Context: pin_num: {}, color_num: {}, duplicate: {}",
-        pin_num, color_num, duplicate
-    );
+    // parse command line arguments
+    let cli = Args::parse();
+    let context = Context {
+        color_num: cli.color_num,
+        pin_num: cli.pin_num,
+        duplicate: !cli.non_duplicate,
+    };
+    println!("{:?}", context);
     println!(
         "You decide the secret code with colors from 0 to {}, and I will guess it. Let's start!",
-        color_num - 1
+        context.color_num - 1
     );
 
-    // manually enumeration of all codes
-    let all_codes: CodeSet = vec![vec![0, 1], vec![0, 2], vec![1, 0], vec![1, 2], vec![2, 0], vec![2, 1]];
+    // enumerate all codes
+    let all_codes = get_all_codes(&context);
 
     // main process
     let mut candidates = all_codes.clone();
     while candidates.len() > 1 {
         let guess = policy(&candidates);
         let hint = trial(&guess);
-        candidates.retain(|x| calc_hint(x, &guess) == hint); // update candidates
+        candidates.retain(|x| calc_hint(x, &guess, &context) == hint); // update candidates
     }
     assert_eq!(candidates.len(), 1);
 
     // post process
     let secret = &candidates[0];
     println!("Your secret is {:?}", secret);
+}
+
+// enumerate all codes according to context
+fn get_all_codes(context: &Context) -> CodeSet {
+    match context.duplicate {
+        true => (0..context.pin_num)
+            .map(|_| 0..context.color_num)
+            .multi_cartesian_product()
+            .collect(),
+        false => (0..context.color_num)
+            .permutations(context.pin_num)
+            .collect(),
+    }
 }
 
 // select guess from guess set
@@ -55,12 +88,12 @@ fn trial(guess: &Code) -> Hint {
 }
 
 // calculate hint from two codes
-fn calc_hint(code: &Code, guess: &Code) -> Hint {
+fn calc_hint(code: &Code, guess: &Code, context: &Context) -> Hint {
     let mut hit = 0;
     let mut blow = 0;
-    let mut code_color_counts = [0; 3]; // array of the number of i-th colors which code has
-    let mut guess_color_counts = [0; 3]; // array of the number of i-th colors which guess has
-    for i in 0..2 {
+    let mut code_color_counts = vec![0; context.color_num]; // array of the number of i-th colors which code has
+    let mut guess_color_counts = vec![0; context.color_num]; // array of the number of i-th colors which guess has
+    for i in 0..context.pin_num {
         if code[i] == guess[i] {
             hit += 1;
         } else {
@@ -68,7 +101,7 @@ fn calc_hint(code: &Code, guess: &Code) -> Hint {
             guess_color_counts[guess[i]] += 1;
         }
     }
-    for i in 0..3 {
+    for i in 0..context.color_num{
         blow += std::cmp::min(code_color_counts[i], guess_color_counts[i]);
     }
     (hit, blow)
